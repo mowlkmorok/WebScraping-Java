f6aa574fa5fe0361cb7f4062a362a8605f32cd27
mowlkmorok <mowlklk69@gmail.com>
2021-06-16 13:13:57 -0300

    The first project webscraping java

---
+++ src/main/java/br/com/lf/wsapi/WebScrapingJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lf.wsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrapingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScrapingJavaApplication.class, args);
+	}
+
+}
